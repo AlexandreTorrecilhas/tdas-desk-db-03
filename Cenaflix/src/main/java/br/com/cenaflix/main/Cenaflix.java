e3eaf2acf93088e5c080cec37b16a974d20cd293
AlexandreTorrecilhas <carlos.torrecilhas@outlook.com>
2025-01-21 21:34:46 -0300

    Subindo o projeto pela primeira vez. Apenas o projeto foi criado, nada foi inicilizado

---
+++ Cenaflix/src/main/java/br/com/cenaflix/main/Cenaflix.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.cenaflix.main;
+
+/**
+ *
+ * @author carlo
+ */
+public class Cenaflix {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
