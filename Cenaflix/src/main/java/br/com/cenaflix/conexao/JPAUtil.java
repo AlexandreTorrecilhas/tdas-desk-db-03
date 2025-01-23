3fb7c34088d95a37dd8cdf2d5e9a2dba70140b6c
Carlos Alexandre Torrecilhas da Silva Sá <carlos.sa@amebarradas.org.br>
2025-01-23 15:53:49 -0300

    Criamos a classe JPAUtil. Ela é responsável por criar o EntityFactoryManager e o EntityManager

---
+++ Cenaflix/src/main/java/br/com/cenaflix/conexao/JPAUtil.java
@@ -0,0 +1,31 @@
+package br.com.cenaflix.conexao;
+
+//Pacotes Jakartas
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+    
+    private static final String PERSISTENCE_UNIT = "Cenaflix";
+    private static EntityManagerFactory cenaflixFabrica;
+    private static EntityManager em;
+    
+    public static EntityManager getEntityManager(){
+        if(cenaflixFabrica == null || !cenaflixFabrica.isOpen()){
+            cenaflixFabrica = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        }
+        if(em == null || !em.isOpen()){
+            em = cenaflixFabrica.createEntityManager();
+        }
+        
+        return em;
+    }
+    
+    public static void closeEntityManager(){
+        if(em != null && em.isOpen()){
+            em.close();
+            cenaflixFabrica.close();
+        }
+    }
+}
